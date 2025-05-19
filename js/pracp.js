108e59d3a511ea84b885eaeb4721fddcbda909ed
dragon <yonguru0501@gmail.com>
2025-05-20 00:28:38 +0900

    4

--- js/pracp.js
+++ js/pracp.js
@@ -2,3 +2,13 @@ $('nav > ul > li').hover(function(){
     $('.sub').stop().slideDown()}, 
     function(){$('.sub').stop().slideUp()}
 );
+
+function slide(){
+    $('.swrap').animate({marginLeft:'-100%'},1000,function(){
+    $(this).append($('.swrap li').first());
+    $(this).css({marginLeft:0});
+    });
+};
+setInterval (slide, 3000);
+
+
